008824ed0eb336a155b1da5cc563fcb1fbe67503
Vincenzo Donnarumma <vincenzodonnarumma22@gmail.com>
2024-12-19 13:48:54 +0100

    [FIX]: delete unnecessary code

--- src/Controller/PurchaseController.php
+++ src/Controller/PurchaseController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Service\PurchaseManagerService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
